b744f4d539bed43ea2bef584864ed4b8377e314a
sheng chen <sche3154@uni.sydney.edu.au>
2024-05-17 20:51:19 +1000

    day2

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,4 +1,10 @@
 # todo_list/todo_app/urls.py
+from django.urls import path
+from . import views
+
+app_name = "todo_app"
 
 urlpatterns = [
+    path("",views.ToDoListView.as_view(),name="index"),
+    path('list/<int:list_id>/', views.ItemListView.as_view(), name = "list"),
 ]
